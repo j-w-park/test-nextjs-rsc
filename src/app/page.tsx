ddcedee3c311788054738429a0432220ceb7fae8
j-w-park <okml133@gmail.com>
2023-05-14 16:06:09 +0900

    feat: render todo list in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-import TodoList from './TodoList';
-import './page.css';
+import { db } from '@/repository/db';
+import Item from './TodoItem';
+import { Todo } from './api/types';
+import styles from './page.module.css';
 
-export default async function Home() {
+export const dynamic = 'force-dynamic';
+
+export default async function TodoList() {
+  const { rows: todos } = await db.query<Todo>('SELECT * FROM todos');
+  console.log('number of rows: ', todos.length);
   return (
-    <main>
-      <TodoList />
+    <main className={styles['.container-main']}>
+      <h1>{new Date().toISOString()}</h1>
+      <ul className={styles['container-todo']}>
+        {todos.map((todo) => (
+          <Item key={todo.id} todo={todo} />
+        ))}
+      </ul>
     </main>
   );
 }
