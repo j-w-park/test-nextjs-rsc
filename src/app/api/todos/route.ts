1e27bfc2f453f1cc97c2544d8399fd7e829ee732
j-w-park <okml133@gmail.com>
2023-05-14 14:50:31 +0900

    feat: api

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,6 +1,15 @@
 import { db } from '@/lib/db';
 import { Todo } from '../types';
 
+export async function GET() {
+  try {
+    const { rows } = await db.query<Todo>('SELECT * FROM todos');
+    return new Response(JSON.stringify(rows), { status: 200 });
+  } catch (e) {
+    return new Response(JSON.stringify(e), { status: 500 });
+  }
+}
+
 export async function POST(request: Request) {
   const body = (await request.json()) as Pick<Todo, 'description' | 'title'>;
   try {
@@ -8,8 +17,8 @@ export async function POST(request: Request) {
       'INSERT INTO todos (title, description, status) VALUES ($1, $2, $3)',
       [body.title, body.description, 'NOT_STARTED'],
     );
+    return new Response(null, { status: 201 });
   } catch (e) {
     return new Response(JSON.stringify(e), { status: 500 });
   }
-  return new Response(null, { status: 201 });
 }
