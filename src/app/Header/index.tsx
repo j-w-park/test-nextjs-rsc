e31bdf2f7830151ed8773cdd15795a17740493f9
j-w-park <okml133@gmail.com>
2023-05-14 17:02:10 +0900

    feat: render menu

--- src/app/Header/index.tsx
+++ src/app/Header/index.tsx
@@ -1,6 +1,28 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+
+interface Menu {
+  readonly title: string;
+  readonly href: string;
+}
 
 export const Header = () => {
+  const pathname = usePathname();
+
+  const menus: ReadonlyArray<Menu> = useMemo(() => {
+    switch (pathname) {
+      case '/':
+        return [{ title: 'Todo', href: '/todo' }];
+      case '/todo':
+        return [{ title: 'Add', href: '/todo/add' }];
+      default:
+        return [];
+    }
+  }, [pathname]);
+
   return (
     <aside id="app-header">
       <h1>
@@ -10,11 +32,13 @@ export const Header = () => {
       </h1>
       <nav>
         <ul id="app-header-container-link">
-          <li>
-            <Link className="app-header-link" href="./todo">
-              Todo
-            </Link>
-          </li>
+          {menus.map((menu) => (
+            <li key={menu.href}>
+              <Link className="app-header-link" href={menu.href}>
+                {menu.title}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </aside>
