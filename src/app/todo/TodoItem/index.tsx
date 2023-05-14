87c8147769880e19d6a62ebd18c19f7d6664a8f2
j-w-park <okml133@gmail.com>
2023-05-14 19:21:43 +0900

    feat: render other fields

--- src/app/todo/TodoItem/index.tsx
+++ src/app/todo/TodoItem/index.tsx
@@ -1,9 +1,26 @@
 import { Todo } from '@/app/api/types';
+import { format } from 'date-fns';
 
 interface Props {
   todo: Todo;
 }
 
 export const TodoItem = (props: Props) => {
-  return <p>{props.todo.title}</p>;
+  const formattedCreatedAt = format(
+    props.todo.created_at,
+    'yyyy-MM-dd HH:mm:ss',
+  );
+  const formattedUpdatedAt = format(
+    props.todo.updated_at,
+    'yyyy-MM-dd HH:mm:ss',
+  );
+  return (
+    <li className="item">
+      <h2 id="title">{props.todo.title}</h2>
+      <p id="description">{props.todo.description}</p>
+      <p id="status">{props.todo.status}</p>
+      <small id="created_at">CREATED: {formattedCreatedAt}</small>
+      <small id="updated_at">UPDATED: {formattedUpdatedAt}</small>
+    </li>
+  );
 };
