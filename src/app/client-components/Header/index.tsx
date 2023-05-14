ec35d7c37d3050e68806723bf7c39e5b9cd4c779
j-w-park <okml133@gmail.com>
2023-05-14 16:51:53 +0900

    feat: home link

--- src/app/client-components/Header/index.tsx
+++ src/app/client-components/Header/index.tsx
@@ -3,11 +3,15 @@ import Link from 'next/link';
 export const Header = () => {
   return (
     <aside id="app-header">
-      <h1>React Server Component Test</h1>
+      <h1>
+        <Link className="app-header-link" href="/">
+          React Server Component Test
+        </Link>
+      </h1>
       <nav>
         <ul id="app-header-container-link">
           <li>
-            <Link id="app-header-link" href="./todo">
+            <Link className="app-header-link" href="./todo">
               Todo
             </Link>
           </li>
